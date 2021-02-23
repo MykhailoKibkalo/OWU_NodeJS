8c410920167f41682ddd782c31621a7350b15abf
Mykhailo <mixa450@gmail.com>
2021-02-23 14:50:05 +0200

    add ls&path

--- less3/app.js
+++ less3/app.js
@@ -1,2 +1,8 @@
-const ls = require('ls');
+const express = require('express');
 const path = require('path');
+const apiRouter = require('./router/api.router');
+
+
+const app = express();
+
+app.use('/', apiRouter);
