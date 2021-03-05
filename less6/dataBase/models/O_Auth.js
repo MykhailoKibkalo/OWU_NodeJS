45ea9deefbb44282fb40d35dfd4a8a12c94f519f
Mykhailo <mixa450@gmail.com>
2021-03-05 16:01:08 +0200

    fixed hw 1.1

--- less6/dataBase/models/O_Auth.js
+++ less6/dataBase/models/O_Auth.js
@@ -4,7 +4,7 @@ const { dataBaseTablesEnum: { O_AUTH, USER } } = require('../../constant');
 const oAuthScheme = new Schema({
     access_token: { type: String },
     refresh_token: { type: String },
-    user: { type: Schema.Types.ObjectId, ref: USER },
+    _user_id: { type: Schema.Types.ObjectId, ref: USER },
 }, { timestamps: true });
 
 module.exports = model(O_AUTH, oAuthScheme);
